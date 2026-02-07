61219f0a5171812e74da1ac3d41a693c6c16a656
Jobia Damone <jobiadamone@hotmail.com>
2026-02-07 09:05:59 -0300

    recfactor: padronizado o componente do Panel de acordo com os demais.

--- src/components/Panel/Panel.tsx
+++ src/components/Panel/Panel.tsx
@@ -1,42 +1,6 @@
-import styled from 'styled-components';
-import type { PanelProps } from './Panel.types';
+import { PanelStyle } from "./Panel.styles";
+import { PanelProps } from "./Panel.types";
 
-export const Panel = styled.div<PanelProps>`
-  background-color: ${({ theme, variant }) => theme.colors[variant]};
-  border-radius: ${({ theme }) => theme.borderRadius.xl};
-  padding: ${({ theme }) => theme.spacing.lg};
-  justify-content: ${({ direction = 'center' }) => direction};
-  align-items: center;
-  margin-bottom: ${({ overlap = 0 }) => `-${overlap}px`};
-  width: ${({ fullWidth }) => (fullWidth ? '100%' : 'auto')};
-
-  display: flex;
-  flex-direction: row;
-  height: 7.5rem;
-  gap: 50px;
-
-  p {
-    color: ${({ theme }) => theme.colors.textPrimary};
-    font-size: ${({ theme }) => theme.fontSize.lg};
-    font-weight: 500;
-    width: 40%;
-  }
-
-  @media (max-width: 1040px) {
-    height: 12rem;
-    flex-direction: column;
-    justify-content: end;
-    align-items: flex-start;
-    padding-bottom: ${({ overlap = 0 }) => (overlap > 0 ? '50px' : '0px')};
-    gap: 20px;
-
-    p {
-      font-size: ${({ theme }) => theme.fontSize.md};
-      width: 100%;
-      margin: 0;
-    }
-  }
-  @media (max-width: 390px) {
-    padding-bottom: ${({ overlap = 0 }) => (overlap > 0 ? '35px' : '0px')};
-  }
-`;
+export const Panel = ({ children, ...props }: PanelProps) => {
+  return <PanelStyle {...props}>{children}</PanelStyle >;
+};
--- src/components/Panel/Panel.types.tsx
+++ src/components/Panel/Panel.types.tsx
@@ -1,8 +1,10 @@
+import { ReactNode } from 'react';
 import type { DefaultTheme } from 'styled-components';
 
 type ColorVariant = keyof DefaultTheme['colors'];
 
 export interface PanelProps {
+  children: ReactNode,
   variant: ColorVariant;
   direction?: 'left' | 'right';
   overlap?: number;
